626cf027177b5bd15f5da286d09697b034ef6265
Chris Anderson <jchris@couchbase.com>
2013-12-12 09:26:51 -0800

    example secrets

---
+++ StreamFavorites/AppSecretConfig.h
@@ -0,0 +1,24 @@
+//
+//  AppSecretConfig.h
+//  CBLIUpdateBrowser
+//
+//  Created by Chris Anderson on 12/5/13.
+//  Copyright (c) 2013 Chris Anderson. All rights reserved.
+//
+
+// HINT HINT: Add this file to .gitignore right now before you forget. :)
+
+#ifndef StreamFavorites_AppSecretConfig_h
+#define StreamFavorites_AppSecretConfig_h
+
+#define kSyncRemoteServer @"http://10.0.1.12:8080"
+#define kSyncRemoteDB @"listream"
+
+// the URL LinkedIn will redirect the WebView to
+#define kLIRedirectURL @"http://mobile.couchbase.com"
+//the app id you get from the registered LinkedIn application
+#define kLIClientID @"xxxxx"
+//the client secret you get from the registered LinkedIn application
+#define kLIClientSecret @"xxxxxx"
+
+#endif
